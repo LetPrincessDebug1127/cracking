3e3c27f674e9ad33dbc0fe6ce92833f09654dbe6
NganSoSweet <phamngocthungan20030@gmail.com>
2024-10-19 22:49:07 +0700

    debug : rv module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/auth/users.module'; // Đảm bảo đường dẫn đúng
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Tải cấu hình từ file .env
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/',
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+        Logger.log(`Connecting to MongoDB at URI: ${uri}`, 'MongoDBConnection');
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
+    UsersModule, // Đăng ký UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-
