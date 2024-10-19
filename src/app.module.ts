4c29ae281c2914d13e397ed52fab24e6f5d5d024
NganSoSweet <phamngocthungan20030@gmail.com>
2024-10-19 23:08:35 +0700

    update module user and module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,27 +5,27 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/auth/users.module'; // Đảm bảo đường dẫn đúng
+import { UsersModule } from './users/auth/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // Tải cấu hình từ file .env
+    ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'), // Đường dẫn tới thư mục public
-      serveRoot: '/', // Đường dẫn gốc để phục vụ tệp tĩnh
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
         const uri = configService.get<string>('MONGO_URI');
         Logger.log(`Connecting to MongoDB at URI: ${uri}`, 'MongoDBConnection');
-        return { uri }; // Trả về URI để kết nối
+        return { uri };
       },
-      inject: [ConfigService], // Tiêm ConfigService
+      inject: [ConfigService],
     }),
-    UsersModule, // Đăng ký UsersModule
+    UsersModule,
   ],
-  controllers: [AppController], // Đăng ký controllers
-  providers: [AppService], // Đăng ký providers
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/users/auth/users.module.ts
+++ src/users/auth/users.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersController } from './user.controller'; // Đảm bảo đường dẫn đúng
-import { UsersService } from './users.service'; // Đảm bảo đường dẫn đúng
-import { User, UserSchema } from './user.schema'; // Đảm bảo đường dẫn đúng
+import { UsersController } from './user.controller';
+import { UsersService } from './users.service';
+import { User, UserSchema } from './user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), // Đăng ký schema cho User
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [UsersController], // Đăng ký controller
-  providers: [UsersService], // Đăng ký service
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
